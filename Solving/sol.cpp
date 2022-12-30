d088c6dada4b8d5c6cdbd8ef08eb701e0557a4d7
Saken Serik <sakenserik07@gmail.com>
2022-12-30 14:49:46 +0600

    29 Dec

--- Solving/sol.cpp
+++ Solving/sol.cpp
@@ -1,34 +1,25 @@
-#include<bits/stdc++.h>
-#define sz(x) (int)(x).size()
-#define pb push_back
-#define mp make_pair
-#define f first
-#define s second
-                
-typedef long long ll;
-
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-	string a, b;
-	cin >> a >> b;
-	vector<vector<int>> dp(sz(a) + 1, vector<int> (sz(b) + 1, 10000));
-	dp[0][0] = 0;
-	for (int i = 0; i <= sz(a); i++) {
-		for (int j = 0; j <= sz(b); j++) {
-			if (i) dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
-			if (j) dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
-			if (i && j) dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + (a[i - 1] != b[j - 1]));
-		}
-	}
-	cout << dp[sz(a)][sz(b)];
-}
+int lis = 0;
+pair<int, int> a[100000];
+set<int> s;
 
 int main() {
-	ios::sync_with_stdio(0);
+	iostream::sync_with_stdio(false);
 	cin.tie(0);
-
-	solve();
-	
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> a[i].first >> a[i].second;
+	sort(a, a + n, greater<pair<int, int>>());
+	cout << '\n';
+	for (int i = 0; i < n; i++) cout << a[i].first << ' ' << a[i].second << '\n';
 	return 0;
-} 
+	// finding the LIS of a reversed array = finding the LDS
+	for (int i = 0; i < n; i++) {
+		if (s.upper_bound(a[i].second) == s.end()) lis++;
+		else s.erase(s.upper_bound(a[i].second));
+		s.insert(a[i].second);
+	}
+	cout << lis;
+}

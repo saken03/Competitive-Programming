798d602e50891a53a0920ef09d89e6575ee926a1
Saken Serik <sakenserik07@gmail.com>
2023-01-21 00:14:46 +0600

    20 Dec

--- Solving/sol.cpp
+++ Solving/sol.cpp
@@ -10,39 +10,24 @@ using namespace std;
 
 typedef long long ll;
 
+const int INF = (int)1e10;
+
 void solve() {
-	int n, x;
-	cin >> n >> x;
-	vector<int> a(n);
-	for (int i = 0; i < n; i++) cin >> a[i];
-	
-	// dp[msk] = {minimum number of rides, w is total weight of last ride
-	// msk is persons we take overall      
-	vector<pair<int, int>> dp(1 << n, mp(n + 1, 121212112));
-	dp[0] = mp(1, 0);
-	for (int i = 0; i < n; i++) 
-		if (a[i] <= x) dp[1 << i] = mp(1, a[i]);
-	for (int msk = 1; msk < (1 << n); msk++) {
-		for (int i = 0; i < n; i++) {
-		  if (msk & (1 << i)) {
-		  	pair<int, int> prev = dp[msk ^ (1 << i)];
-		  	int w = prev.s, r = prev.f;
-		  	if (w + a[i] <= x) w += a[i];
-		  	else r += 1, w = a[i];   
-		  	dp[msk] = min(dp[msk], mp(r, w));
-		  }
-		  /*
-			if ((msk & (1 << i)) == 0) {
-				int nmsk = msk | (1 << i);
-				if (dp[msk].s + a[i] <= x) 
-					dp[nmsk] = min(dp[nmsk], mp(dp[msk].f, dp[msk].s + a[i]));
-				else 
-					dp[nmsk] = min(dp[nmsk], mp(dp[msk].f + 1, a[i]));
+	string s;
+	cin >> s;
+	int n = sz(s);
+	vector<vector<int>> dp(505, vector<int> (505));
+	for (int j = 0; j <= n; j++) {
+		for (int i = 0; i < n - j; i++) {
+			dp[i][i + j] = dp[i + 1][i + j] + 1;
+			for (int k = i + 1; k <= i + j; k++) {
+				if (s[i] == s[k]) {
+					dp[i][i + j] = min(dp[i][i + j], dp[i + 1][k - 1] + dp[k + 1][i + j]);
+				}
 			}
-			*/
 		}
 	}
-	cout << dp[(1 << n) - 1].f << '\n';
+	cout << dp[0][n - 1] << '\n';
 }
 
 int main() {
--- USACO/train/Chapter 1/Section 1.4/Prime Cryptarithm/crypt1.cpp
+++ USACO/train/Chapter 1/Section 1.4/Prime Cryptarithm/crypt1.cpp
@@ -1,5 +1,5 @@
 /***
-PROB: milk
+PROB: crypt1
 LANG: C++14
 username: sakenseri1
 ***/
@@ -29,12 +29,94 @@ void setIO(string name = "") {
 	}
 }
 
+int n, num[100500];
+bool has[10];
+
+bool mult(int a, int b, int c, int d, int e) {
+	vector<int> p1(4), p2(4), p3(5);
+	p1[0] = c * e;
+	p1[1] = b * e;
+	p1[2] = a * e;
+	p2[0] = c * d;
+	p2[1] = b * d;
+	p2[2] = a * d;
+
+	p1[1] += p1[0] / 10;
+	p1[0] %= 10;
+	p1[2] += p1[1] / 10;
+	p1[1] %= 10;
+	p1[3] += p1[2] / 10;
+	p1[2] %= 10;
+
+	p2[1] += p2[0] / 10;
+	p2[0] %= 10;
+	p2[2] += p2[1] / 10;
+	p2[1] %= 10;
+	p2[3] += p2[2] / 10;
+	p2[2] %= 10;
+
+	for (int i = 0; i < 4; i++) {
+		if (i == 3 && p1[i] == 0) continue;
+		if (i == 3 && p2[i] == 0) continue;
+		if (!has[p1[i]] || !has[p2[i]])
+			return 0;
+	} 
+
+	p3[0] = p1[0];
+	for (int i = 1; i < 4; i++) {
+		p3[i] = p1[i] + p2[i - 1];
+	}
+	p3[4] = p2[3];
+
+	p3[1] += p3[0] / 10;
+	p3[0] %= 10;
+	p3[2] += p3[1] / 10;
+	p3[1] %= 10;
+	p3[3] += p3[2] / 10;
+	p3[2] %= 10;
+	p3[4] += p3[3] / 10;
+	p3[3] %= 10;
+
+	for (int i = 0; i < 5; i++) {
+	  if (i == 4 && p3[i] == 0) continue;
+		if (!has[p3[i]]) 
+			return 0;    
+	}
+
+	if (p1[3] != 0 || p2[3] != 0 || p3[4] != 0) return 0;
+	return 1;
+}
+
 void solve() {
-	int n;
 	cin >> n;
-	vector<int> s(n);
-	for (int i = 0; i < n; i++) cin >> s[i];
-	                    
+
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+		has[num[i]] = 1;
+	}                              
+	
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				for (int l = 0; l < n; l++) {
+					for (int m = 0; m < n; m++) {
+						int a, b, c, d, e;
+						a = num[i];
+						b = num[j];
+						c = num[k];
+						d = num[l];
+						e = num[m];
+						if (mult(a, b, c, d, e)) {
+//							cout << a << ' ' << b << ' ' << c << ' ' << d << ' ' << e << '\n';
+							cnt++;
+						}
+					}
+				}
+			}
+		}
+	}
+	cout << cnt << '\n';
 }
 
 int main() {
